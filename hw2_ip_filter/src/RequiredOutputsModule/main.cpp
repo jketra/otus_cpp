b97d36ff12c1210fb29f328f0a2b04dea7e985e2
Julia S. Goloschapova <js@goloschapova.com>
2020-04-16 23:30:57 +0300

    Remove test sstream from RequiredOutputsModule

--- hw2_ip_filter/src/RequiredOutputsModule/main.cpp
+++ hw2_ip_filter/src/RequiredOutputsModule/main.cpp
@@ -8,37 +8,10 @@
 #include <string>
 #include <iostream>
 
-#define JDEBUG
-#ifdef  JDEBUG
-	#include <fstream>
-	#include <sstream>
-#endif
-
 int main(int argc, char const *argv[])
 {
 	(void)argc;
 	(void)argv;
-
-#ifndef JDEBUG
-	auto inStream = std::cin;
-#else
-	std::stringstream inStream;
-	#ifdef WIN32
-		std::string filePath{ "D:\\study\\OTUS\\HW2\\input_part.txt" };
-	#else
-		std::string filePath{ "/home/user/code/test_data/input_part.txt" };
-	#endif
-
-	std::ifstream file(filePath);
-
-	if (file) {
-		inStream << file.rdbuf();
-		file.close();
-	}
-	else {
-		std::cout << "Could not open file " << filePath << std::endl;
-	}
-#endif
 	
 	try {
 		constexpr auto DESC = bl::PrintDirection::DESC;
@@ -46,7 +19,7 @@ int main(int argc, char const *argv[])
 		bl::IpStorage ipStorage;
 		bl::IpStoragePrinter printer(std::cout);
 
-		for (std::string line; std::getline(inStream, line);)
+		for (std::string line; std::getline(std::cin, line);)
 		{
 			std::vector<std::string> v = bl::split(line, '\t');
 			if ((!v.empty()) && (!v.front().empty())) {
@@ -62,17 +35,11 @@ int main(int argc, char const *argv[])
 		// 1. Полный список адресов после обратной сортировки.
 		printer.print<DESC>(ipStorage.getAllIps());
 
-		std::cout << "\n=====================================================\n";
-
 		// 2. Список адресов, первый байт которых равен 1. Порядок сортировки не меняется.
 		printer.print<DESC>(ipStorage.filteredByFirstBytes(1));
 
-		std::cout << "\n=====================================================\n";
-
 		// 3. Список адресов, первый байт которых равен 46, а второй 70. Порядок сортировки не меняется.
 		printer.print<DESC>(ipStorage.filteredByFirstBytes(46, 70));
-
-		std::cout << "\n=====================================================\n";
 
 		// 4. Список адресов, любой байт которых равен 46. Порядок сортировки не меняется.
 		printer.print<DESC>(ipStorage.getIpsContainsByte(46));
