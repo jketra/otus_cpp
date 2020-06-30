05fc989d4145a3e0e4fe85ded6cc6c87b34d9d41
Julia Goloschapova <js@goloschapova.com>
2020-06-30 19:35:53 +0300

    hw4: printIp methods logic and example

--- hw4_print_ip/src/PrintIp/PrintIp.h
+++ hw4_print_ip/src/PrintIp/PrintIp.h
@@ -1,13 +1,98 @@
 ﻿#pragma once
 
 #include <iostream>
+#include <vector>
+#include <iterator>
+#include <type_traits>
+#include <string>
+#include <utility>
 
-namespace hw4
-{
+#include <TypeTraits/TypeTraits.h>
+
+namespace hw4 {
+
+template<typename T, typename = std::enable_if_t<std::is_integral_v<T>>>
+void printIp(T ip) {
+	
+	Bits<T> mask;
+	mask.value = std::numeric_limits<uint8_t>::max(); // 2^8 - 1;
+
+	std::vector<uint8_t> result;
+	result.reserve(sizeof(T));
+
+	for (int k = 0; k < sizeof(T); ++k) {
+		T newValue = mask.value & ip;
+		newValue = newValue >> (k * 8);
+		result.emplace_back(static_cast<uint8_t>(newValue));
+		mask.value = mask.value << 8;
+	}
+
+	for (auto it = result.rbegin();;) {
+		std::cout << static_cast<uint32_t>(*it);
+		++it;
+		
+		if (it == result.rend()) {
+			break;
+		}
+
+		std::cout << '.';
+	}
+
+	std::cout << std::endl;
+}
+
+void printIp(const std::string& ip);
+
+template<typename T, typename = sfinae::IsContainer<T>>
+void printIp(const T& ip) {
+	if (ip.empty()) {
+		return;
+	}
+
+	for (auto it = ip.begin();;) {
+		std::cout << *it;
+		++it;
+
+		if (it == ip.end()) {
+			break;
+		}
+
+		std::cout << '.';
+	}
+
+	std::cout << std::endl;
+}
+
+template<class Tuple, std::size_t... Indexes>
+void print_tuple_impl(const Tuple& t, std::index_sequence<Indexes...>, const char* delimeter) {
+	int tmp[]{0, (void(std::cout << (Indexes == 0 ? "" : delimeter) << std::get<Indexes>(t)), 0)...};
+	(void)tmp;
+}
+
+template<typename T, typename... Args, typename = sfinae::AreTypesSame<T, Args...>>
+void printIp(const std::tuple<T, Args...>& ip) {
+	print_tuple_impl(ip, std::index_sequence_for<T, Args...>{}, ".");
+	std::cout << std::endl;
+}
 
 template<typename T>
-void printIp(T ip) {
-	std::cout << ip << std::endl;
+union Bits {
+	T value;
+	uint8_t bits[sizeof(T)];
+};
+
+template<typename T>
+void show_bits(T a) {
+	Bits<T> b;
+	b.value = a;
+	
+	for (int j = sizeof(T) - 1; j >= 0; j--) {
+		for (int i = 128; i; i >>= 1)
+			if (i & b.bits[j]) std::cout << "1";
+			else std::cout << "0";
+		std::cout << ' ';
+	}
+	std::cout << std::endl;
 }
 
 }
--- libs/TypeTraits/TypeTraits.h
+++ libs/TypeTraits/TypeTraits.h
@@ -20,6 +20,15 @@ struct disjunction<B1, Bn...>
 
 namespace sfinae {
 
+template<typename ...>
+using void_t = void;
+
+template<typename T, typename = void>
+struct is_container : std::false_type {};
+
+template<typename T>
+struct is_container<T, void_t<typename std::remove_reference_t<T>::iterator>> : std::true_type {};
+
 template<typename Expected, typename Actual>
 using CheckType = typename std::enable_if_t< std::is_same< std::remove_const_t<std::remove_reference_t<Actual>>, 
 std::remove_const_t<std::remove_reference_t<Expected>> >::value >;
@@ -27,4 +36,9 @@ std::remove_const_t<std::remove_reference_t<Expected>> >::value >;
 template<typename Expected, typename... Actual>
 using CheckTypes = typename std::enable_if_t<fake_std17::conjunction<std::is_convertible<Actual, Expected>...>::value>;
 
+template<typename T>
+using IsContainer = typename std::enable_if_t<sfinae::is_container<std::remove_reference_t<T>>::value>;
+
+template<typename Expected, typename... Actual>
+using AreTypesSame = typename std::enable_if_t<fake_std17::conjunction<std::is_same<Actual, Expected>...>::value>;
 }
