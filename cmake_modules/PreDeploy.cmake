347e88a8d34ee32500be108db0c426af3bf923a1
Julia Goloschapova <js@goloschapova.com>
2020-07-03 19:43:09 +0300

    CMake: set default deploy as none + other minor changes

--- cmake_modules/PreDeploy.cmake
+++ cmake_modules/PreDeploy.cmake
@@ -1,4 +1,4 @@
-﻿set(DEFAULT_DEPLOY_PROJECT "${PRINT_IP}")
+﻿set(DEFAULT_DEPLOY_PROJECT "none")
 
 set(DEPLOY_PROJECT "${DEFAULT_DEPLOY_PROJECT}" CACHE STRING "${DEFAULT_DEPLOY_PROJECT}")
 
@@ -8,7 +8,13 @@ set(DEPLOY_${CICD} OFF)
 set(DEPLOY_${IP_FILTER} OFF)
 set(DEPLOY_${PRINT_IP} OFF)
 
-if (NOT ${DEPLOY_PROJECT} STREQUAL "none" AND BUILD_${DEPLOY_PROJECT})
-	set(DEPLOY_${DEPLOY_PROJECT} ON)
-	message(STATUS "Release package will be created for ${DEPLOY_PROJECT} project.")
+if (NOT ${DEPLOY_PROJECT} STREQUAL "none") 
+	if(BUILD_${DEPLOY_PROJECT})
+		set(DEPLOY_${DEPLOY_PROJECT} ON)
+		message(STATUS "Release package will be created for ${DEPLOY_PROJECT} project.")
+	else()
+		message("Project for deploy (${DEPLOY_PROJECT}) can't be OFF.")
+		set(DEPLOY_${DEPLOY_PROJECT} OFF)
+		set(DEPLOY_PROJECT "none" CACHE STRING "none")
+	endif()
 endif()
