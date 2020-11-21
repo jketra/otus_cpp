a50fa9f88b3177e9d834dc09e5a1dde2a425a283
Julia S. Goloschapova <js@goloschapova.com>
2020-11-21 21:23:55 +0300

    Allocator. Simple ReserveAllocator + Tests

--- hw2_allocator/ReserveAllocator/ReserveAllocator.h
+++ hw2_allocator/ReserveAllocator/ReserveAllocator.h
@@ -10,19 +10,6 @@
 
 namespace hw2 {
 
-//template<typename T>
-//struct ReserveAllocator;
-//
-//template<>
-//struct ReserveAllocator<void> {
-//    using pointer = void*;
-//    using const_pointer = const void*;
-//    using value_type = void;
-//
-//    template<typename U>
-//    using other = ReserveAllocator<U>;
-//};
-
 template<typename T/*, size_t size_*/>
 struct ReserveAllocator {
     using value_type = T;
@@ -41,40 +28,30 @@ struct ReserveAllocator {
         using other = ReserveAllocator<U>;
     };
 
-    //template<typename U>
-    //using other = ReserveAllocator<U>;
-
-    //pointer allocate(size_type n, ReserveAllocator<void>::const_pointer hint = nullptr) const {
-    pointer allocate(size_type n, const_void_pointer hint = 0) const {
+    pointer allocate(size_type n, const_void_pointer hint = nullptr) const {
         std::cout << __PRETTY_FUNCTION__ << "[n = " << n << "]" << std::endl;
-        //auto p = std::malloc(n * sizeof(T));
-        //if (!p)
-        //    throw std::bad_alloc();
-        //return reinterpret_cast<T*>(p);
         (void)hint;
         return reinterpret_cast<T*>(::operator new(n * sizeof(T)));
-        //return reinterpret_cast<T*>(new char[n * sizeof(T)]);
     }
 
     void deallocate(T* p, std::size_t n) const {
         std::cout << __PRETTY_FUNCTION__ << "[n = " << n << "]" << std::endl;
-        //std::free(p);
 
         delete[] reinterpret_cast<char*>(p);
     }
 
-    //template<typename U, typename ...Args>
-    //void construct(U* p, Args &&...args) const {
-    //    std::cout << __PRETTY_FUNCTION__ << std::endl;
-    //    new(p) U(std::forward<Args>(args)...);
-    //}
-//
-    //template< class U >
-    //void destroy(U* p) {
-    //    std::cout << __PRETTY_FUNCTION__ << std::endl;
-    //    ((T*)p)->~T();
-    //    p->~U();
-    //}
+    template<typename U, typename ...Args>
+    void construct(U* p, Args &&...args) const {
+        std::cout << __PRETTY_FUNCTION__ << std::endl;
+        new(p) U(std::forward<Args>(args)...);
+    }
+
+    template< class U >
+    void destroy(U* p) {
+        std::cout << __PRETTY_FUNCTION__ << std::endl;
+        ((T*)p)->~T();
+        p->~U();
+    }
 
     // NEED COPY CONSTRUCTOR !!!!!!!!!
 };
